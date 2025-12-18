53758060db42d9836be739c389980bb3a1b947e3
Subrata Ghosh <subrataghosh8228@gmail.com>
2025-12-18 19:38:27 +0530

    improvement done

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,6 +1,33 @@
 package pages;
 
 import base.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage extends BaseTest {
+
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+
+    }
+
+    public void tournamentPage() {
+
+        WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(10));
+        WebElement tournament =wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("(//div[@class='h-48 overflow-hidden'])[1]")
+        ));
+        tournament.click();
+
+        System.out.println("Tournament page clicked");
+
+    }
 }
--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -1,4 +1,18 @@
 package tests;
 
-public class HomePageTest {
+import base.BaseTest;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class HomePageTest extends BaseTest {
+
+
+    @Test(priority = 6)
+    public void HomePageTest()  {
+        HomePage hpt = new HomePage(driver);
+        hpt.tournamentPage();
+
+    }
+
+
 }
